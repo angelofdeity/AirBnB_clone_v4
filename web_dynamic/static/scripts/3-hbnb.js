063c62002e6e01d9070b6cda7ab333d894e25cb5
Nico <lawani.gk@gmail.com>
2023-05-05 09:56:01 +0100

    updats js file to fetch places from api

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -5,15 +5,48 @@ $.get(statusApi, (data, textStatus) => {
   }
 });
 const searchPlacesUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
-const places = document.querySelector('section.places');
 fetch(searchPlacesUrl, {
   method: 'POST',
   headers: {
-    'Content-type': 'application/json'
+    'Content-type': 'application/json',
   },
-  body: JSON.stringify({})
+  body: JSON.stringify({}),
 })
   .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+  .then((places) => {
+    const placesSection = document.querySelector('section.places');
+    for (const place of places) {
+      placesSection.appendChild(createPlaceArticle(place));
+    }
+  })
+  .catch((error) => console.error('Failed to fetch places:', error));
+
+function createPlaceArticle(place) {
+  const article = document.createElement('article');
+  const titleBox = document.createElement('div');
+  titleBox.className = 'title_box';
+  const placeName = document.createElement('h2');
+  placeName.textContent = place.name;
+  const priceByNight = document.createElement('div');
+  priceByNight.className = 'price_by_night';
+  priceByNight.textContent = '$' + place.price_by_night;
+  titleBox.append(placeName, priceByNight);
+
+  const info = document.createElement('div');
+  info.className = 'information';
+  const maxGuest = document.createElement('div');
+  maxGuest.className = 'max_guest';
+  maxGuest.textContent = place.max_guest + ' Guest(s)';
+  const numberRooms = document.createElement('div');
+  numberRooms.className = 'number_rooms';
+  numberRooms.textContent = place.number_rooms + ' Bedroom(s)';
+  const numberBathrooms = document.createElement('div');
+  numberBathrooms.className = 'number_bathrooms';
+  numberBathrooms.textContent = place.max_guest + ' Bathroom(s)';
+  info.append(maxGuest, numberRooms, numberBathrooms);
+
+  const description = document.createElement('div');
+  description.innerHTML = place.description;
+  article.append(titleBox, info, description);
+  return article;
+}
