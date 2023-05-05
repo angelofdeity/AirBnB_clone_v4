0bb4de163adeb625c948213cfa4c0666f3aa8ba0
Nico <lawani.gk@gmail.com>
2023-05-05 13:08:54 +0100

    add gitignore

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,18 @@
-const api = 'http://0.0.0.0:5001/api/v1/status/';
-$.get(api, (data, textStatus) => {
-  if (textStatus) {
-    $('div#api_status').addClass('available');
-  }
+$(() => {
+  const amenityDict = {};
+  $("input[type='checkbox']").on('click', event => {
+    const id = event.target.dataset.id;
+    if (event.target.checked) {
+      amenityDict[id] = event.target.dataset.name;
+    } else {
+      delete amenityDict[id];
+    }
+    $('.amenities h4').text(Object.values(amenityDict));
+  });
+  const api = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(api, (data, textStatus) => {
+    if (textStatus) {
+      $('div#api_status').addClass('available');
+    }
+  });
 });
