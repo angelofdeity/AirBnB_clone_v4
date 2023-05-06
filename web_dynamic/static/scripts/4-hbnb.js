f7a1983a2765b07f08fce7fe2cb369d98f4b5b97
Nico <lawani.gk@gmail.com>
2023-05-06 19:31:37 +0100

    refactor code for better readability

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,9 +1,10 @@
 const searchPlacesUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
 const amenityDict = {};
 document.addEventListener('DOMContentLoaded', () => {
+  checkApiStatus();
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   const amenities = document.querySelector('.amenities h4');
-  function handleCheckbox (checkbox) {
+  function handleCheckbox(checkbox) {
     const id = checkbox.dataset.id;
     if (checkbox.checked) {
       amenityDict[id] = checkbox.dataset.name;
@@ -16,24 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
     handleCheckbox(checkbox);
     checkbox.addEventListener('click', () => handleCheckbox(checkbox));
   });
-  function updateAmenities () {
+  function updateAmenities() {
     amenities.textContent = Object.values(amenityDict).join(', ');
   }
   updateAmenities();
-});
-$(() => {
-  const statusApi = 'http://0.0.0.0:5001/api/v1/status/';
-  fetch(statusApi).then((response) => {
-    if (response.ok) {
-      $('div#api_status').addClass('available');
-    }
-  });
-  fetchPlaces({});
-});
-
-document.addEventListener('DOMContentLoaded', () => {
+  // Filter place By Amenities
   const searchButton = document.querySelector('button');
-  console.log(searchButton);
   const placesSection = document.querySelector('section.places');
   searchButton.addEventListener('click', () => {
     const amenities = { amenities: Object.keys(amenityDict) };
@@ -41,6 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchPlaces(amenities);
   });
 });
+function checkApiStatus () {
+  const statusApi = 'http://0.0.0.0:5001/api/v1/status/';
+  fetch(statusApi).then((response) => {
+    if (response.ok) {
+      $('div#api_status').addClass('available');
+    }
+  });
+  fetchPlaces({});
+}
 
 function fetchPlaces (data) {
   fetch(searchPlacesUrl, {
